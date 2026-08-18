3c9002ff481e9364eeda79d18e9ce678bfdb1254
Mauricio Rey Hernández Curiel <mau.rhcl@gmail.com>
2026-08-18 16:22:17 -0600

    Add: two-integer-sum - submission-2

---
+++ Data Structures & Algorithms/two-integer-sum/submission-2.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        unordered_map<int, int> complement;
+        for (int i = 0; i < nums.size(); i++) {
+            int remain = target - nums[i];
+            if (complement.contains(remain)) {
+                return {complement[remain], i};
+            }
+            complement[nums[i]] = i;
+        }
+    }
+};
