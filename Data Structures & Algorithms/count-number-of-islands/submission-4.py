2abc5810a5076701f1199705a3f86e8a859e2bf5
Mauricio Rey Hernández Curiel <mau.rhcl@gmail.com>
2026-08-26 08:57:26 -0600

    Add: count-number-of-islands - submission-4

---
+++ Data Structures & Algorithms/count-number-of-islands/submission-4.py
@@ -0,0 +1,54 @@
+class Solution:
+    def numIslands(self, grid: List[List[str]]) -> int:
+        """
+        rows, cols = len(grid), len(grid[0])
+
+        def dfs(r, c):
+            if grid[r][c] == "1":
+                grid[r][c] = "0"
+                if r > 0:
+                    dfs(r - 1, c)
+                if r < rows - 1:
+                    dfs(r + 1, c)
+                if c > 0:
+                    dfs(r, c - 1)
+                if c < cols - 1:
+                    dfs(r, c + 1)
+
+        tot = 0
+        for r in range(rows):
+            for c in range(cols):
+                if grid[r][c] == "1":
+                    tot += 1
+                    dfs(r, c)
+        return tot
+        """
+        rows, cols = len(grid), len(grid[0])
+        tot = 0
+        visited = set()
+        def bfs(r,c):
+            q = collections.deque()
+            visited.add((r,c))
+            q.append((r,c))
+
+            while q:
+                row, col = q.popleft()
+                directions = ((1,0), (-1, 0), (0, 1), (0, -1))
+                
+                for dr, dc in directions:
+                    r, c = row + dr, col + dc
+                    if (r in range(rows) and
+                        c in range(cols) and
+                        grid[r][c] == "1" and
+                        (r, c) not in visited):
+                        q.append((r, c))
+                        visited.add((r, c))
+
+
+        for r in range(rows):
+            for c in range(cols):
+                if grid[r][c] == "1" and (r,c) not in visited:
+                    tot += 1 
+                    bfs(r,c)
+                    
+        return tot
