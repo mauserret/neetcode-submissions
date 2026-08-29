a94f4497b7d94c83bbd6ed58aa29ec05c76d310e
Mauricio Rey Hernández Curiel <mau.rhcl@gmail.com>
2026-08-29 12:05:56 -0600

    Add: search-insert-position - submission-3

---
+++ Data Structures & Algorithms/search-insert-position/submission-3.py
@@ -0,0 +1,16 @@
+class Solution:
+    def searchInsert(self, nums: List[int], target: int) -> int:
+        l, r = 0, len(nums) - 1
+
+        while l <= r:
+            m = (r + l) // 2
+            print(m)
+            mid = nums[m]
+            if mid == target:
+                return m
+            if mid > target:
+                r = m - 1
+            else:
+                l = m + 1
+        return l 
+        
